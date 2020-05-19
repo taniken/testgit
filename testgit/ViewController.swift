50e415592ef49a396cc1fd52f4f9df96742030bf
kenichi.tani <kenichi.tani@toyoko-inn.com>
2020-05-19 15:21:37 +0900

    Initial Commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Toyoko on 2020/05/19.
+//  Copyright © 2020 Toyoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
